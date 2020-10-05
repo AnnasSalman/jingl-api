65a8e7a1e27abdba390db7942dc8be5fdfbef906
AnnasSalman <annassalman.98@gmail.com>
2020-10-05 21:46:00 +0500

    CORS policy updated

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors({origin: 'https://jingl-video-converter.herokuapp.com'}));
 
 
 app.use('/', indexRouter);
